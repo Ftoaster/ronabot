346bc2d10af2c83767ff4a6dd750cb907aa2bada
Ftoaster <32581364+Ftoaster@users.noreply.github.com>
2020-11-18 16:35:39 +0900

    Update Bot.java

--- discordbot/src/main/java/Bot.java
+++ discordbot/src/main/java/Bot.java
@@ -1,14 +1,21 @@
-import events.HelloEvent;
+import events.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class Bot {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        JDA jda = JDABuilder.createDefault("Nzc3ODY5Njg4OTY5MTAxMzUy.X7JtmQ.FLBYnAEB-8WuPSOnj951A3W-UcU").build();
+        JDA jda = JDABuilder.createDefault("Nzc3ODY5Njg4OTY5MTAxMzUy.X7JtmQ.ViPZ6jL2bhppHP5s-HaonasJWMc").build();
 
         jda.addEventListener(new HelloEvent());
-    }
+        jda.addEventListener(new madye());
+        jda.addEventListener(new AdvItem());
+        jda.addEventListener(new Help());
+        jda.addEventListener(new veteran());
 
+    }
 }
+
+
+
